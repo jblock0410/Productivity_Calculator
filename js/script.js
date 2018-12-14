702b3a11e5a709065063c65dc3c6364ec849dca6
Little Bit <jeffreyblock@outlook.com>
2018-12-13 18:51:22 -0600

    Update new script file

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
-/*
-let button = document.querySelector("button");
-let inputField = document.getElementById("test");
-button.addEventListener("click", addInputField => {
-    if (addInputField.button == 0) {
-        console.log("Button Pressed Once");
-        inputField.innerHTML = "This actually worked!";
-    } else if (addInputField.button == 1) {
-        console.log("Button Pressed Twice");
-        inputField.innerHTML = "This actually worked again!";
+const ul = document.querySelector('ul');
+
+
+
+
+function addItemButton1() {
+    let li = document.createElement('li');
+    let input = document.querySelector('.strategicInput');
+    let ul = document.getElementById('strategicUl');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+}
+
+function addItemButton2() {
+    let li = document.createElement('li');
+    let input = document.querySelector('.teamInput');
+    let ul = document.getElementById('teamUl');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+}
+
+function addItemButton3() {
+    let li = document.createElement('li');
+    let input = document.querySelector('.meetingsInput');
+    let ul = document.getElementById('meetingsUl');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+}
+
+
+function addItemButton (button) {
+    let buttonId = button.id;
+    switch (buttonId) {
+        case 'strategic': 
+            addItemButton1(buttonId);
+            break;
+        case 'team':
+            addItemButton2(buttonId);
+            break;
+        case 'meetings':
+            addItemButton3(buttonId);
+            break;
+        default:
+            return false;
     }
 }
-);
-*/
 
-let button = document.querySelector("button");
-let inputField = document.getElementById("test");
-button.addEventListener("click", addInputField => {
-    console.log("Push One");
-    inputField.innerHTML = "Push One";  
-});
-
-
-
-
-
+const buttons = document.getElementsByTagName('button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+        addItemButton(this);
+    }
+}
